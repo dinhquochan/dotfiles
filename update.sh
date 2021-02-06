0449644203d8a85cd4a29d4ea3296ecf51b1ac8e
Dinh Quoc Han <contact@dinhquochan.com>
2021-02-07 02:18:14 +0700

    🚀 switch to bash

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cp ~/.zshrc .
+cp ~/.bashrc .
+cp ~/.bash_profile .
 cp ~/.tmux.conf .
 cp ~/.vimrc .
 cp ~/.phpcs.xml .
+cp ~/.php_cs .
 cp ~/.gitignore_global .
 cp ~/Dropbox/sublime-text/User/Preferences.sublime-settings sublime-settings.json
 
