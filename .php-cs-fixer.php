f2192a93d91648cb9e64c3b25d6ed6de15f59dfa
Dinh Quoc Han <handinh@hey.com>
2021-05-20 11:33:22 +0700

    update php-cs-fixer for psr12

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,11 +5,13 @@ $finder = PhpCsFixer\Finder::create()
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->ignoreVCS(true)
+;
 
-return PhpCsFixer\Config::create()
-    ->setRules([
-        '@PSR2' => true,
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
+        '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => [
             'default' => 'single_space',
@@ -17,8 +19,6 @@ return PhpCsFixer\Config::create()
                 '=' => null,
             ],
         ],
-        'blank_line_after_opening_tag' => true,
-        'blank_line_before_return' => true,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
@@ -26,21 +26,17 @@ return PhpCsFixer\Config::create()
             'spacing' => 'one',
         ],
         'function_typehint_space' => true,
-        'lowercase_cast' => true,
-        'lowercase_static_reference' => true,
         'native_function_casing' => true,
         'no_extra_blank_lines' => true,
-        'no_leading_import_slash' => true,
         'no_leading_namespace_whitespace' => true,
         'no_spaces_around_offset' => true,
         'no_unused_imports' => true,
-        'no_whitespace_in_blank_line' => true,
         'not_operator_with_successor_space' => true,
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
         'object_operator_without_whitespace' => true,
         'single_quote' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'unary_operator_spaces' => true,
         'whitespace_after_comma_in_array' => true,
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+;
