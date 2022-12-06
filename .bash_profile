4988b1e44abe07c060bf4d0baa87cb842c7184f8
Dinh Quoc Han <dqh@dinhquochan.com>
2022-12-06 11:53:09 +0700

    Move to zsh again

--- .bash_profile
+++
@@ -1,6 +0,0 @@
-if [ -r ~/.bashrc ]; then
-   source ~/.bashrc
-fi
-
-[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
-
--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-cp ~/.bash_profile .
-cp ~/.bashrc .
-cp ~/.bash_aliases .
+cp ~/.zshrc .
 cp ~/.tmux.conf .
 cp ~/.vimrc .
 cp ~/.php-cs-fixer.php .
