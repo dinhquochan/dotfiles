d46cece706a029c034478ba46bfe967722ff927c
Dinh Quoc Han <dqh@dinhquochan.com>
2025-06-02 18:30:36 +0700

    update

--- update.sh
+++ update.sh
@@ -6,8 +6,7 @@ cp ~/.config/tmux/tmux.conf config/tmux/tmux.conf
 cp ~/.zshrc zshrc
 cp ~/.config/git/ignore config/git/ignore
 cp ~/.config/git/message config/git/message
-cp -R ~/.config/kitty/* config/kitty/*
+cp -R ~/.config/kitty/* config/kitty/
 cp ~/.gitconfig gitconfig
 
 brew bundle dump --force
-
